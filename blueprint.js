1c51b9f64269e040809e818183c389a808181d45
mindspank <alec.karlsson@gmail.com>
2015-10-25 22:52:43 -0400

    wrapping apply in promise

--- blueprint.js
+++ blueprint.js
@@ -4,64 +4,73 @@ var getBlueprint = require('./lib/get-blueprint');
 var METHODS = require('./lib/assemble-blueprint');
 var resolveDeletions = require('./lib/resolve-deletions');
 
-function applyTo(applist, blueprint, config) {	
+function applyTo(applist, blueprint, config) {
 	return Promise.each(applist, function(app) {
 		return apply(app, blueprint, config)
+	})
+	.catch(function(error) {
+		console.log(error)
 	})
 	.done()
 };
 
 function apply(appid, blueprint, config) {
-	config.appname = appid;
-	
-	// qsocks content cache.
-	var $ = {};
-	
-	var SUPPORTED_OBJECT_TYPES = ['story', 'sheet', 'measure', 'dimension', 'masterobject', 'snapshot'];
-	var METHODS_MAP = ['sheets', 'measures', 'dimensions', 'masterobjects', 'snapshots', 'stories'];
+	return new Promise(function(resolve, reject) {
+		var c = config;
+		c.appname = appid;
+		// qsocks content cache.
+		var $ = {};
 			
-	return qsocks.Connect(config).then(function(global) {
-			return $.global = global;
-		}).then(function() {
-			return $.global.openDoc(appid, '', '', '', true);
-		})
-		.then(function(app) {
-			return $.app = app;
-		})
-		.then(function(app) {
-			return $.app.getAllInfos()
-		})
-		.then(function(info) {
-			return info.qInfos.filter(function(obj) {
-				return SUPPORTED_OBJECT_TYPES.indexOf(obj.qType) !== -1;
-			}).map(function(zip) {
-				return zip.qId;
-			});
-		})
-		.then(function(appObjectList) {
-			return Promise.each(METHODS_MAP, function(method) {
-				if (blueprint[method] && blueprint[method].length) {
-					return Promise.each(blueprint[method], function(definition) {
-						return METHODS[method]($.app, definition, appObjectList)
-					})		
-				} else {
-					return Promise.resolve();					
-				}
+		var SUPPORTED_OBJECT_TYPES = ['story', 'sheet', 'measure', 'dimension', 'masterobject', 'snapshot'];
+		var METHODS_MAP = ['sheets', 'measures', 'dimensions', 'masterobjects', 'snapshots', 'stories'];
+				
+		return qsocks.Connect(c).then(function(global) {
+				return $.global = global;
+			}).then(function() {
+				return $.global.openDoc(appid, '', '', '', true);
 			})
-		})
-		/*.then(function() {
-			return resolveDeletions($.app, blueprint);
-		})*/
-		.then(function() {
-			return $.app.saveObjects()
-		}).then(function() {
-			// Clean up and free up connections.
-			$.global.connection.ws.terminate()
-			return $ = null;
-		})
-		.catch(function(error) {
-			console.log(error)
-		}).done();
+			.then(function(app) {
+				return $.app = app;
+			})
+			.then(function(app) {
+				return $.app.getAllInfos()
+			})
+			.then(function(info) {
+				return info.qInfos.filter(function(obj) {
+					return SUPPORTED_OBJECT_TYPES.indexOf(obj.qType) !== -1;
+				}).map(function(zip) {
+					return zip.qId;
+				});
+			})
+			.then(function(appObjectList) {
+				return Promise.each(METHODS_MAP, function(method) {
+					if (blueprint[method] && blueprint[method].length) {
+						return Promise.each(blueprint[method], function(definition) {
+							console.log(METHODS[method])
+							return METHODS[method]($.app, definition, appObjectList)
+						})		
+					} else {
+						return Promise.resolve();					
+					}
+				})
+			})
+			.then(function() {
+				return resolveDeletions($.app, blueprint);
+			})
+			.then(function() {
+				return $.app.saveObjects()
+			}).then(function() {
+				// Clean up and free up connections.
+				$.global.connection.ws.terminate()
+				return $ = null;
+			})
+			.then(function() {
+				return resolve();
+			})
+			.catch(function(error) {
+				return reject(error)
+			}).done();
+	})
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bp = require('./blueprint')
 
 bp.getBlueprint('f97c71d8-301c-429a-9992-c0af8044dec4', config.engine)
 .then(function (blueprint) {
-	return bp.applyTo(['82050482-cafa-40d3-8468-df640c98b7c2'], blueprint, config.engine)
+	return bp.applyTo(['12b6352a-866c-4ef1-bd3d-0908000cbcad', '0e374626-60ec-41c4-a221-395498f8a590'], blueprint, config.engine)
 })
 .catch(function (error) {
 	console.log(error)
